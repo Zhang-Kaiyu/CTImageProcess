34fa8b40899500be50d4a5078f6386ad27a1819b
Zhang-Kaiyu <41471941+Zhang-Kaiyu@users.noreply.github.com>
2020-08-04 15:34:46 +0800

    Add files via upload
    DCM图像的读取显示，以及DCM转png；
    opencv连通域分析；

---
+++ CTImageProcess.cpp
@@ -0,0 +1,156 @@
+﻿// CTImageProcess.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+//CT图像格式转换，DCM转png
+#include <iostream>
+#include <string>
+
+//包含DCMTK 头文件和lib  
+
+#include <dcmtk/config/osconfig.h>
+#include <dcmtk/dcmdata/dctk.h>
+#include <dcmtk/dcmdata/dcrleccd.h>
+#include <dcmtk/dcmimgle/dcmimage.h>
+#include <dcmtk/dcmimage/diregist.h>
+#include <dcmtk/dcmjpeg/djdecode.h>
+
+//#pragma comment(lib, "Netapi32.lib")  
+//#pragma comment(lib, "ws2_32.lib") 
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+using namespace std;
+
+void loadDCMImage();
+void loadDCMFile();
+int main()
+{
+	/*std::cout << "Hello World!\n";
+	Mat im = imread("123.jpg");
+	imshow("test", im);
+	waitKey();   */
+
+//	loadDCMImage();
+	loadDCMFile();
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+//显示一片黑
+void loadDCMImage()
+{
+	string name("9d7f4df008702001000.dcm");
+
+	DcmFileFormat fileFormat;
+	OFCondition oc = fileFormat.loadFile(name.c_str());
+
+	if (!oc.good())//判断文件是否读取成功
+	{
+		std::cerr << "文件路径有问题" << std::endl;
+		return;
+	}
+
+	DcmDataset* dataSet = fileFormat.getDataset();
+	E_TransferSyntax xfer = dataSet->getCurrentXfer();//得到传输语法
+
+	OFString patientName;
+	dataSet->findAndGetOFString(DCM_BitsStored, patientName);//获取病人姓名
+	std::cout << "patientName :" << patientName << std::endl;
+
+	unsigned short bitCount(0);
+	dataSet->findAndGetUint16(DCM_BitsStored, bitCount);//获取像素的位数 bit
+	cout << "bit_count :" << bitCount << endl;
+
+	OFString isRGB;
+	dataSet->findAndGetOFString(DCM_PhotometricInterpretation, isRGB);//DCM图片的图像模式
+	std::cout << "isrgb :" << isRGB << std::endl;//monochrome 单色
+
+	unsigned short imgBits(0);
+	dataSet->findAndGetUint16(DCM_SamplesPerPixel, imgBits);//单个像素占用多少byte
+	std::cout << "img_bits :" << imgBits << std::endl;
+
+	unsigned short m_width;
+	unsigned short m_height;
+	dataSet->findAndGetUint16(DCM_Rows, m_height);
+	dataSet->findAndGetUint16(DCM_Columns, m_width);
+	cout << "width :" << m_width << endl;//364
+	cout << "height " << m_height << endl;//268
+
+	DcmElement* element = nullptr;
+	OFCondition result = dataSet->findAndGetElement(DCM_PixelData, element);
+	if (result.bad() | element == nullptr)
+	{
+		std::cerr << "获取像素数据失败!" << std::endl;
+	}
+
+	uchar* image8Data = nullptr;
+	Uint16* image16Data = nullptr;
+	if (8 == bitCount)
+	{
+		result = element->getUint8Array(image8Data);
+	}
+	else if (8 < bitCount)
+	{
+		result = element->getUint16Array(image16Data);
+	}
+
+	if (image8Data)
+	{
+		if (1 == imgBits)
+		{
+			cv::Mat dst(m_height, m_width, CV_8UC1, cv::Scalar::all(0));
+			unsigned char* data = nullptr;
+			for (int i = 0; i < m_height; i++)
+			{
+				data = dst.ptr<unsigned char>(i);	//取得每一行的头指针 也可使用dst2.at<unsigned short>(i, j) = ?
+				for (int j = 0; j < m_width; j++)
+				{
+					*data++ = (unsigned char)((float)image8Data[i*m_width + j] * 255.0 / std::pow(2.0, bitCount) + 0.5);
+				}
+			}
+			cv::imshow("gray", dst);
+			cv::waitKey();
+		}
+	}
+	else if (image16Data)
+	{
+		DicomImage* m_dcmImage = new DicomImage((DcmObject*)dataSet, xfer);
+		m_dcmImage->setWindow(268, 364);//这句话很重要，没有的话会一片黑
+		Uint16* pixelData=(Uint16*)(m_dcmImage->getOutputData(16));
+		if (pixelData != nullptr)
+		{
+			cv::Mat dst2(m_height, m_width, CV_16UC1, pixelData);
+			cv::imshow("image", dst2);
+			cv::waitKey(0);
+		}
+	}
+}
+
+void loadDCMFile()
+{
+	DicomImage *img = new DicomImage("9d7f4df008703011190.dcm");
+	int nWidth = img->getWidth();			//获得图像宽度
+	int nHeight = img->getHeight();			//获得图像高度
+//	img->setWindow(450, 1600);	//肝脏一般取窗宽为450HU，窗位为45HU，这边的窗宽和窗位要自己设定
+	img->setWindow(500, 8000);
+	Uint16 *pixelData = (Uint16*)(img->getOutputData(16));
+	std::cout << nWidth << ", " << nHeight << std::endl;
+	if (pixelData != nullptr)
+	{
+		cv::Mat dst(nHeight, nWidth, CV_16UC1, pixelData);
+		cv::resize(dst, dst, cv::Size(dst.cols*2., dst.rows * 2));
+		cv::imshow("image2", dst);
+		imwrite("008703011190.png", dst);
+		cv::waitKey(0);
+	}
+}
---
+++ connectedAnalysis.cpp
@@ -0,0 +1,159 @@
+﻿//// connectedAnalysis.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+////
+//
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//
+//using namespace std;
+//using namespace cv;
+//
+//int main()
+//{
+//	Mat im = imread("../CTImageProcess/008703011190.png");
+//	namedWindow("image190");
+//	imshow("image190", im);
+//	waitKey();
+//    std::cout << "Hello World!\n";
+//}
+//
+//// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+//// 调试程序: F5 或调试 >“开始调试”菜单
+//
+//// 入门使用技巧: 
+////   1. 使用解决方案资源管理器窗口添加/管理文件
+////   2. 使用团队资源管理器窗口连接到源代码管理
+////   3. 使用输出窗口查看生成输出和其他消息
+////   4. 使用错误列表窗口查看错误
+////   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+////   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+RNG rng(12345);
+
+void connected_component_demo(Mat &image);
+
+void connected_component_stats_demo(Mat &image);
+
+int main(int argc, char** argv) {
+	Mat src = imread("../CTImageProcess/008703011190.png");
+	if (src.empty()) {
+		printf("could not load image...\n");
+	}
+	imshow("input", src);
+//	connected_component_stats_demo(src);
+	connected_component_demo(src);
+	waitKey(0);
+	return 0;
+}
+
+void connected_component_demo(Mat &image) {
+	// 二值化
+	Mat gray, binary;
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+	threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+	// 形态学操作
+	Mat k = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
+	morphologyEx(binary, binary, MORPH_OPEN, k);
+	morphologyEx(binary, binary, MORPH_CLOSE, k);
+	imshow("binary", binary);
+	imwrite("../temp/ccla_binary.png", binary);
+	Mat labels = Mat::zeros(image.size(), CV_32S);
+	int num_labels = connectedComponents(binary, labels, 8, CV_32S);
+	printf("total labels : %d\n", (num_labels - 1));
+	vector<Vec3b> colors(num_labels);
+	// background color
+	colors[0] = Vec3b(0, 0, 0);
+
+	// object color
+	for (int i = 1; i < num_labels; i++) {
+		colors[i] = Vec3b(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+	}
+
+	// render result
+	Mat dst = Mat::zeros(image.size(), image.type());
+	int w = image.cols;
+	int h = image.rows;
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			int label = labels.at<int>(row, col);
+			if (label == 0) continue;
+			dst.at<Vec3b>(row, col) = colors[label];
+		}
+	}
+	imshow("ccla-demo", dst);
+	imwrite("./temp/ccla_dst.png", dst);
+}
+
+
+
+void connected_component_stats_demo(Mat &image) {
+	// 二值化
+	Mat gray, binary;
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+	threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+	// 形态学操作
+	Mat k = getStructuringElement(MORPH_RECT, Size(3, 3), Point(-1, -1));
+	morphologyEx(binary, binary, MORPH_OPEN, k);
+	morphologyEx(binary, binary, MORPH_CLOSE, k);
+	imshow("binary", binary);
+
+	Mat labels = Mat::zeros(image.size(), CV_32S);
+	Mat stats, centroids;
+	int num_labels = connectedComponentsWithStats(binary, labels, stats, centroids, 8, 4);
+	printf("total labels : %d\n", (num_labels - 1));
+	vector<Vec3b> colors(num_labels);
+
+	// background color
+	colors[0] = Vec3b(0, 0, 0);
+
+	// object color
+	int b = rng.uniform(0, 256);
+	int g = rng.uniform(0, 256);
+	int r = rng.uniform(0, 256);
+
+	for (int i = 1; i < num_labels; i++) {
+		colors[i] = Vec3b(0, 255, 0);
+	}
+
+	// render result
+	Mat dst = Mat::zeros(image.size(), image.type());
+
+	int w = image.cols;
+	int h = image.rows;
+
+	for (int row = 0; row < h; row++) {
+		for (int col = 0; col < w; col++) {
+			int label = labels.at<int>(row, col);
+			if (label == 0) continue;
+			dst.at<Vec3b>(row, col) = colors[label];
+		}
+	}
+
+	for (int i = 1; i < num_labels; i++) {
+		Vec2d pt = centroids.at<Vec2d>(i, 0);
+		int x = stats.at<int>(i, CC_STAT_LEFT);
+		int y = stats.at<int>(i, CC_STAT_TOP);
+
+		int width = stats.at<int>(i, CC_STAT_WIDTH);
+		int height = stats.at<int>(i, CC_STAT_HEIGHT);
+
+		int area = stats.at<int>(i, CC_STAT_AREA);
+		printf("area : %d, center point(%.2f, %.2f)\n", area, pt[0], pt[1]);
+
+		circle(dst, Point(pt[0], pt[1]), 2, Scalar(0, 0, 255), -1, 8, 0);
+		rectangle(dst, Rect(x, y, width, height), Scalar(255, 0, 255), 1, 8, 0);
+	}
+
+	imshow("ccla-demo", dst);
+	imwrite("./temp/ccla_stats_dst.png", dst);
+}
+
+
